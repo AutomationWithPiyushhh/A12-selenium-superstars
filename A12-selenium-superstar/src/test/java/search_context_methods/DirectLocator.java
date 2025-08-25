1f6f61fedda3304051d5e41e2dd4b42292c72b38
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-08-25 21:31:49 +0530

    DL

--- A12-selenium-superstar/src/test/java/search_context_methods/DirectLocator.java
+++ A12-selenium-superstar/src/test/java/search_context_methods/DirectLocator.java
@@ -1,22 +1,29 @@
 package search_context_methods;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DirectLocator {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+
 		WebDriver driver = new ChromeDriver();
 
-		driver.get("");
+		driver.get("https://www.amazon.in/");
 
-		By by = By.id("email");
+//		driver.findElement(By.linkText("Forgot password?")).click();
 
-		WebElement un = driver.findElement(by);
+//		driver.findElement(By.className("_8esh")).click();
+		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		System.out.println(links.size());
+		
+		Thread.sleep(3000);
 
-		un.sendKeys("1234567890");
+		driver.quit();
 
 	}
 }
