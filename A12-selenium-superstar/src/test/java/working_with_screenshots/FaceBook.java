b23952c1d078b4982f3beff3459a6612dca125d8
AutomationWithPiyush <automation.with.piyush@gmail.com>
2025-09-12 18:10:15 +0530

    SS done

--- A12-selenium-superstar/src/test/java/working_with_screenshots/FaceBook.java
+++ A12-selenium-superstar/src/test/java/working_with_screenshots/FaceBook.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 
@@ -16,19 +18,28 @@ public class FaceBook {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
-		driver.get("https://www.amazon.in/");
+		driver.get("https://www.facebook.com/");
 
 		Thread.sleep(5000);
-		
+
+//		Taking ss of web page
 		TakesScreenshot tks = (TakesScreenshot) driver;
 		File src = tks.getScreenshotAs(OutputType.FILE);
+		File dest = new File(
+				"./errorShots/webpage.png");
+
+		FileHandler.copy(src, dest);
+
+//		Taking ss of web element => Login button
+
+		WebElement facebook = driver.findElement(By.cssSelector("img[alt=\"Facebook\"]"));
+		File source = facebook.getScreenshotAs(OutputType.FILE);
 		File destination = new File(
-				"path of the file.png");
-		
-		FileHandler.copy(src, destination);
-		
+				"./errorShots/webelement.png");
+		FileHandler.copy(source, destination);
+
 		System.out.println("Screenshot taken successfully!!!");
-		
+
 		Thread.sleep(3000);
 		driver.quit();
 	}
